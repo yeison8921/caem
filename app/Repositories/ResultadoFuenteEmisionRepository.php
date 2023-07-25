3e98acd809687317d1899759b847c87388b5457c
Javdo <javdo2@gmail.com>
2023-07-26 09:17:14 +1000

    fix

--- app/Repositories/ResultadoFuenteEmisionRepository.php
+++ app/Repositories/ResultadoFuenteEmisionRepository.php
@@ -634,9 +634,9 @@ class ResultadoFuenteEmisionRepository extends BaseRepository
         $array_resultados['huella_carbono_fuente_torta'] = [$array_labels_fuente, $array_totales_fuente_torta, $array_colores_fuente];
         $array_resultados['huella_carbono_tipo'] = [$array_labels_tipo, $array_totales_tipo, $array_colores_tipo];
         $array_resultados['total_huella_carbono_unidad_produccion'] = $total_huella_carbono_unidad_produccion;
-        $array_resultados['tipo_unidad'] = $data[0]->tipo_unidad;
+        $array_resultados['tipo_unidad'] = $unidades_totales_producidas[0]->tipo_unidad;
         $array_resultados['esta_aprobado'] = $unidades_totales_producidas[0]->esta_aprobado;
-        
+
 
         return $array_resultados;
     }
