f185e9028a9c721618b976e620f1514e589ac603
yeison8921 <yeison8921@gmail.com>
2024-07-05 08:42:39 -0500

    fix login e intentos fallidos

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,7 +8,6 @@ use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use Illuminate\Support\Facades\Redirect;
 
 class LoginController extends Controller
 {
@@ -37,32 +36,26 @@ class LoginController extends Controller
     {
         $this->validateLogin($request);
 
-        // If the class is using the ThrottlesLogins trait, we can automatically throttle
-        // the login attempts for this application. We'll key this by the username and
-        // the IP address of the client making these requests into this application.
-        if (
-            method_exists($this, 'hasTooManyLoginAttempts') &&
-            $this->hasTooManyLoginAttempts($request)
-        ) {
-            $this->fireLockoutEvent($request);
-
-            return $this->sendLockoutResponse($request);
-        }
-
         if ($this->attemptLogin($request)) {
-            if ($request->hasSession()) {
-                $request->session()->put('auth.password_confirmed_at', time());
+
+            if (!$this->guard()->user()->estado) {
+                auth()->logout();
+                return redirect()->route('login')->withErrors(['error' => 'Su cuenta se encuentra bloqueda, por favor contacte al administrador']);
+            } else {
+                if ($request->hasSession()) {
+                    $request->session()->put('auth.password_confirmed_at', time());
+                }
+
+                return $this->sendLoginResponse($request);
             }
-
-            return $this->sendLoginResponse($request);
-        }
-
-        $userState = $this->incrementLoginAttempts($request);
-
-        if ($userState == 0) {
-            return redirect()->route('login')->withErrors(['error' => 'Su cuenta se encuentra bloqueda, por favor contacte al administrador']);
         } else {
-            return $this->sendFailedLoginResponse($request);
+            $userState = $this->incrementLoginAttempts($request);
+
+            if ($userState == 0) {
+                return redirect()->route('login')->withErrors(['error' => 'Su cuenta se encuentra bloqueda, por favor contacte al administrador']);
+            } else {
+                return $this->sendFailedLoginResponse($request);
+            }
         }
     }
 
@@ -83,6 +76,20 @@ class LoginController extends Controller
     }
 
     /**
+     * Attempt to log the user into the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function attemptLogin(Request $request)
+    {
+        return $this->guard()->attempt(
+            $this->credentials($request),
+            $request->boolean('remember')
+        );
+    }
+
+    /**
      * The user has been authenticated.
      *
      * @param  Request  $request
@@ -92,10 +99,10 @@ class LoginController extends Controller
      */
     protected function authenticated(Request $request, $user)
     {
-        if (!$user->estado) {
-            auth()->logout();
-            return redirect()->route('login')->withErrors(['error' => 'Su cuenta se encuentra bloqueda, por favor contacte al administrador']);
-        }
+        $userTemp = User::where('email', $user->email)->first();
+        $userTemp->intentos_fallidos_ingreso = 0;
+        $userTemp->save();
+
         if ($user->rol_id != 2) {
             $this->redirectTo = RouteServiceProvider::RESULTADOS;
         } else {
